57b68a55a0f82f09bfbb066c607aa9a25eb08379
Matheus Kurianski <matheuskurianski@usp.br>
2020-11-23 14:09:10 -0300

    Setup

---
+++ src/main/java/com/kurianski/comidinhasbank/ComidinhasbankApplication.java
@@ -0,0 +1,13 @@
+package com.kurianski.comidinhasbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComidinhasbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComidinhasbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurianski/comidinhasbank/ComidinhasbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurianski.comidinhasbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidinhasbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
